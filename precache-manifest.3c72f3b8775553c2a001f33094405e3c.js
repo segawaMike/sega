9ea00b5a5ff06ff10b1bb3424f131d24325486fc
segawaMike <collinsmicheal256@gmail.com>
2020-09-09 06:28:48 +0300

    Updates

---
+++ precache-manifest.3c72f3b8775553c2a001f33094405e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077de2e6fdfb7cba9a2f8317b893dfbd",
+    "url": "/sega/index.html"
+  },
+  {
+    "revision": "4525220fe12e9462e878",
+    "url": "/sega/static/css/main.0a203f4f.chunk.css"
+  },
+  {
+    "revision": "fce236cf309e7681d43e",
+    "url": "/sega/static/js/2.78b9947e.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/sega/static/js/2.78b9947e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4525220fe12e9462e878",
+    "url": "/sega/static/js/main.f6bda426.chunk.js"
+  },
+  {
+    "revision": "a8c4580685ecba540e4c",
+    "url": "/sega/static/js/runtime-main.cc61d2c4.js"
+  }
+]);
